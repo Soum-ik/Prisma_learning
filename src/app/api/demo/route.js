2f5d3323448cf4537cbc3823abd5fa46cd955222
soum-ik <sarkarsoumik215@gmail.com>
2024-02-27 03:01:50 +0600

    find, create

--- src/app/api/demo/route.js
+++ src/app/api/demo/route.js
@@ -1,28 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const prisma = new PrismaClient();
-
-  //   await prisma.brands.createMany({
-  //     data: [
-  //       {
-  //         brandImg: "abc.jpg",
-  //         brandName: "apple",
-  //       },
-
-  //       {
-  //         brandImg: "abc1.jpg",
-  //         brandName: "apple",
-  //       },
-
-  //       {
-  //         brandImg: "abc2.jpg",
-
-  6; //         brandName: "apple",
-  //       },
-  //     ],
-  //   });
-  let list = await prisma.brands.findMany();
-  return NextResponse.json({ status: list });
+export async function GET(req, res) {
+  // BigInt.prototype.toJSON = function () {
+  //   return this.toString();
+  // };
+  try {
+    const prisma = new PrismaClient();
+    let result = await req.json();
+    const d = await prisma.product.create({ data: result });
+    console.log(data);
+    return NextResponse.json({ status: "Successfull", data: d });
+  } catch (error) {
+    console.log(error);
+  }
 }
