703232de59b321d21c4baeae66acdd543a056792
soum-ik <sarkarsoumik215@gmail.com>
2024-02-27 03:08:31 +0600

    search query

--- src/app/api/quire/route.js
+++ src/app/api/quire/route.js
@@ -1,35 +1,34 @@
 const { PrismaClient } = require("@prisma/client");
 const { NextResponse } = require("next/server");
 
-export async function POST(req, res) {
-  try {
-    const prisma = new PrismaClient();
-    let resBody = await req.json();
+// export async function POST(req, res) {
+//   try {
+//     const prisma = new PrismaClient();
+//     let resBody = await req.json();
 
-    let result = await prisma.product.findMany({
-      where: resBody,
-      select: { title: true, sub_title: true },
-    });
-    return NextResponse.json({
-      status: "Successfull",
-      result: result,
-    });
-  } catch (err) {
-    console.log(err);
-    return NextResponse.json({ status: "error" });
-  }
-}
+//     let result = await prisma.product.findMany({
+//       where: resBody,
+//       select: { title: true, sub_title: true },
+//     });
+//     return NextResponse.json({
+//       status: "Successfull",
+//       result: result,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     return NextResponse.json({ status: "error" });
+//   }
+// }
 export async function GET(req, res) {
   try {
     const prisma = new PrismaClient();
+    // let resBody = await req.json();
 
     let result = await prisma.product.findMany({
-      take: 3,
-      skip: 2,
-      select: {
-        id: true,
-        image: true,
-        createAt: true,
+      where: {
+        image: {
+          contains: "ap",
+        },
       },
     });
     return NextResponse.json({
@@ -41,6 +40,29 @@ export async function GET(req, res) {
     return NextResponse.json({ status: "error" });
   }
 }
+
+// export async function GET(req, res) {
+//   try {
+//     const prisma = new PrismaClient();
+
+//     let result = await prisma.product.findMany({
+//       take: 3,
+//       skip: 2,
+//       select: {
+//         id: true,
+//         image: true,
+//         createAt: true,
+//       },
+//     });
+//     return NextResponse.json({
+//       status: "Successfull",
+//       result: result,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     return NextResponse.json({ status: "error" });
+//   }
+// }
 
 // export async function POST(req, res) {
 //   BigInt.prototype.toJSON = function () {
